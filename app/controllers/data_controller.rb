b5badf2c2e5856d6a6a1c30e79ca145c833ea684
Benjamin <Benjaminkakai001@gmail.com>
2024-05-26 09:10:22 +0300

    Created both the frontend and the backend in the same project folder

---
+++ app/controllers/data_controller.rb
@@ -0,0 +1,22 @@
+require 'net/http'
+require 'json'
+
+class DataController < ApplicationController
+  before_action :authenticate_user!
+
+  def fetch
+    url = URI.parse('https://cat-fact.herokuapp.com/facts')
+    response = Net::HTTP.get_response(url)
+    data = JSON.parse(response.body)
+
+    formatted_data = data.map do |fact|
+      {
+        fact: fact['text'],
+        verified: fact['status']['verified'].to_s,
+        created_at: DateTime.parse(fact['createdAt']).strftime('%d-%m-%Y %H:%M:%S')
+      }
+    end
+
+    render json: formatted_data
+  end
+end
